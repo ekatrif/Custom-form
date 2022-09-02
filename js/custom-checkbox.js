6ac0fbbdb3997225a44d0653f525812fc39a805c
ekatrif <katiapervak@gmail.com>
2022-09-02 13:06:22 +0300

    Добавлен функционал сбора данных формы в объект

--- js/custom-checkbox.js
+++ js/custom-checkbox.js
@@ -1,9 +1,5 @@
 const checkboxInput = document.querySelector(".checkbox input");
 
 checkboxInput.addEventListener("change", function (event) {
-  if (checkboxInput.getAttribute("value") === "false") {
-    checkboxInput.setAttribute("value", "true");
-  } else {
-    checkboxInput.setAttribute("value", "false");
-  }
+  checkboxInput.getAttribute("value") === "false" ? checkboxInput.setAttribute("value", "true") : checkboxInput.setAttribute("value", "false");
 });
--- js/send-form.js
+++ js/send-form.js
@@ -2,17 +2,30 @@ const form = document.getElementById("cvv");
 
 function serializeForm(formNode) {
   console.log(formNode.elements);
-  const formElements = { ...formNode };
+  const formElements = [...formNode];
   //console.log(formElements);
-  for (key in formElements) {
-    const { name, value } = formElements[key];
-    //Обработка кастомного select
-    if (formElements[key].name === "customSelect" && formElements[key].hasAttribute("checked")) {
-      console.log({ name: "framework", value: selectSingleTitle.textContent });
-    } else if (formElements[key].name !== "customSelect") {
-      console.log({ name, value });
-    }
-  }
+  // for (key in formElements) {
+  //   const { name, value } = formElements[key];
+  //   //Обработка кастомного select
+  //   if (formElements[key].name === "customSelect" && formElements[key].hasAttribute("checked")) {
+  //     console.log({ name: "framework", value: selectSingleTitle.textContent });
+  //   } else if (formElements[key].name !== "customSelect") {
+  //     console.log({ name, value });
+  //   }
+
+  // }
+  console.log(formElements);
+  const data = formElements
+    .filter((element) => element.name !== "customSelect")
+    .filter((element) => !!element.name)
+    .map((element) => {
+      const { name, value } = element;
+      if (element.name === "customSelect" && element.hasAttribute("checked")) {
+        return { name: "framework", value: selectSingleTitle.textContent };
+      }
+      return { name, value };
+    });
+  console.log(data);
 }
 form.addEventListener("submit", function (event) {
   event.preventDefault();
