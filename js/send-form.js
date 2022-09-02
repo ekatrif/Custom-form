fa028f000bce0998d43ca821195b231ff0795cb8
ekatrif <katiapervak@gmail.com>
2022-09-02 13:33:29 +0300

    Исправлена ошибка в сборе данных формы в объект

--- js/send-form.js
+++ js/send-form.js
@@ -3,28 +3,20 @@ const form = document.getElementById("cvv");
 function serializeForm(formNode) {
   console.log(formNode.elements);
   const formElements = [...formNode];
-  //console.log(formElements);
-  // for (key in formElements) {
-  //   const { name, value } = formElements[key];
-  //   //Обработка кастомного select
-  //   if (formElements[key].name === "customSelect" && formElements[key].hasAttribute("checked")) {
-  //     console.log({ name: "framework", value: selectSingleTitle.textContent });
-  //   } else if (formElements[key].name !== "customSelect") {
-  //     console.log({ name, value });
-  //   }
-
-  // }
   console.log(formElements);
   const data = formElements
-    .filter((element) => element.name !== "customSelect")
-    .filter((element) => !!element.name)
+
     .map((element) => {
       const { name, value } = element;
       if (element.name === "customSelect" && element.hasAttribute("checked")) {
         return { name: "framework", value: selectSingleTitle.textContent };
+      } else if (element.name === "experience") {
+        return { name, value: !!element.value };
       }
       return { name, value };
-    });
+    })
+    .filter((element) => element.name !== "customSelect")
+    .filter((element) => !!element.name);
   console.log(data);
 }
 form.addEventListener("submit", function (event) {
